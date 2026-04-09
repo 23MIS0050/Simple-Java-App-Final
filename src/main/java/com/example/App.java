02845b7594439e2cd4f5158fde89d64a88f2d06a
23MIS0050 <kaarthik.m2023@vitstudent.ac.in>
2026-04-09 07:46:58 +0530

    Last Commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,4 +1,4 @@
-package src.main.java;
+package com.example;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Java CI/CD Application Started...");
